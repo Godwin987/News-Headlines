bc13c32d235775014d9399d1af5219e1198464fb
Godwin987 <basseygodwin911@gmail.com>
2023-10-12 15:06:33 +0100

    update commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from bs4 import BeautifulSoup
 import time
+import csv
 
 options = Options()
 options.add_argument("headless")
@@ -15,8 +16,23 @@ soup = BeautifulSoup(page_source, "html.parser")
 
 headlines = soup.find(name="div", class_="latest_stories")
 post_title = headlines.find_all(name="h3", class_="post-title")
+
+news = {}
+links = {}
 for post in post_title:
-    print(post.get_text())
+    title = post.get_text()
+    link = post.find_next(name="a")
+    news['Headlines'] = title
+    links['Links'] = link.attrs["href"]
+print(news)
+print(links)
+# with open('news.csv', 'w', newline='') as news:
+#     fieldnames = ['Headlines', 'Links']
+#     writer = csv.DictWriter(news, fieldnames=fieldnames)
+#
+#     writer.writeheader()
+#     writer.writerow({'Headlines': 'Baked', 'Links': 'Beans'})
+
 # print(post_title)
 # for a in headlines:
 #     print()
